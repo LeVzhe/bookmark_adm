24d648e96c2b2f131b14a78e5e677a148bd5cc6e
LeVzhe <levzh93@gmail.com>
2024-12-09 23:14:47 +0300

    fix: hot fix for correct http methods (good expirience :) )

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(http_method_names=["post", "get"]),
+        name="logout",
+    ),
     path("", views.dashboard, name="dashboard"),
 ]
