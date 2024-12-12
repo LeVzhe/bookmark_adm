0ad3ea7b2b9162b8fec3a3dcdcbc796f152627d3
LeVzhe <levzh93@gmail.com>
2024-12-12 22:02:01 +0300

    fix: return old bug with logout in django 5.*

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path(
         "logout/",
-        auth_views.LogoutView.as_view(http_method_names=["post", "get"]),
+        auth_views.LogoutView.as_view(),  # http_method_names=["post", "get", "options"]),
         name="logout",
     ),
     path(
