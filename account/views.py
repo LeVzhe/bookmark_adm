6b699e395dc23c75e7e8c1f2c42ec103b7a2ddd3
LeVzhe <levzh93@gmail.com>
2024-12-12 23:16:33 +0300

    fix: use isort .

--- account/views.py
+++ account/views.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import logout
 
 from .forms import LoginForm
 
